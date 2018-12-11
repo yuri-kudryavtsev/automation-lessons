5f12e32a58d502c258e63692a40c1927a7a41c2a
BuildTools <unconfigured@null.spigotmc.org>
2018-12-11 11:10:54 +0300

    INIT COMMIT

---
+++ src/test/groovy/FirstTest.groovy
@@ -0,0 +1,20 @@
+import org.junit.Test
+
+/**
+ * Created on 11.12.2018
+ *
+ * @author Yuri Kudryavtsev
+ *         skype: yuri.kudryavtsev.indeed
+ *         email: ykudryavtsev@maxilect.com
+ */
+
+class FirstTest {
+    @Test
+    void myFirstTest() {
+        printHello("Yuri Kudryavtsev")
+    }
+
+    static printHello(String userName) {
+        println("Hello, " + userName + "!")
+    }
+}
